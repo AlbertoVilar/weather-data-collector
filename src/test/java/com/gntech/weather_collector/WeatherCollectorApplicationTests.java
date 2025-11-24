ac601ec40f87fcef4a83bc48de66a0090615636d
Alberto Vilar <albertovilar1@gmail.com>
2025-11-24 17:54:37 -0300

    chore: Initial Spring Boot project setup
    - Spring Boot 3.4.12
    - Java 21
    - PostgreSQL Driver
    - Spring Data JPA
    - Spring Web
    - Lombok
    - Actuator
    - Validation
    - Swagger/OpenAPI

---
+++ src/test/java/com/gntech/weather_collector/WeatherCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gntech.weather_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
